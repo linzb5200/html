9da844a1341b3064bec5778c9b477d2e7917fdc3
linzb5200 <linzb5200@gmail.com>
2019-08-25 21:06:02 +0800

    update

--- zb-fly-3.0/res/mods/global.js
+++ zb-fly-3.0/res/mods/global.js
@@ -1,5 +1,16 @@
-;!function(){
-    var layer = layui.layer,form = layui.form;
+
+layui.define(['laypage', 'fly', 'element', 'flow'], function(exports){
+    var $ = layui.jquery;
+    var layer = layui.layer;
+    var util = layui.util;
+    var laytpl = layui.laytpl;
+    var form = layui.form;
+    var laypage = layui.laypage;
+    var fly = layui.fly;
+    var flow = layui.flow;
+    var element = layui.element;
+    var upload = layui.upload;
+
     var mySwiper = new Swiper('.swiper-container',{
         loop: true,
         speed:600,
@@ -16,4 +27,4 @@
 
     })
 
-}();
+});
